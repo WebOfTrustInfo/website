2ec6b799afec6398d188fd760961ded550abb8d2
Shannon Appelcline <shannona@skotos.net>
2023-03-30 15:10:08 -1000

    cookie compliance

---
+++ assets/js/cookie-consent.js
@@ -0,0 +1,12 @@
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#eaf7f7",
+      "text": "#5c7291"
+    },
+    "button": {
+      "background": "#56cbdb",
+      "text": "#ffffff"
+    }
+  }
+});
